822da9e0d96958f1f365282f2ef84ece2cc009de
msineath <msineath@LAPTOP-65OGSQM1.localdomain>
2021-04-07 16:08:55 -0400

    fin

--- mole.js
+++ mole.js
@@ -10,34 +10,49 @@
 //   }, 1000);
 
 // });
+
+let counter = 0;
+let turnsRemaining = 30;
+let timer = 1000;
+
 document.addEventListener("DOMContentLoaded", () => {
+
+  let score = document.getElementById('score');
+  let remaining = document.getElementById('moles');
 
   let heads = document.querySelectorAll(".wgs__molehead")
   heads.forEach(head=> head.addEventListener("click", (event)=>{
     if(event.target.className === "wgs__molehead"){
-      event.target.classList.add("wgs__molehead--hidden", "wgs__molehead--whacked")
+      event.target.classList.add("wgs__molehead--hidden")
+      counter++;
+      score.innerHTML = `Score: ${counter}`;
+      timer = timer - 25;
     }
   }))
 
   function popUpRandomMole() {
-    let moles = document.querySelectorAll(".wgs__molehead:not(.wgs__molehead--whacked)");
-    if(moles.length === 0){
+    let moles = document.querySelectorAll(".wgs__molehead");
+    if(turnsRemaining === 0){
       // const winning = document.createElement('img')
       // winning.src = './youwin.jpg'
       // winning.className = 'end'
       let pf = document.querySelector('.pf')
+      let message = document.getElementById('message');
+      message.innerHTML = "Game Over";
       pf.innerHTML = `<img class='end' src='./youwin.jpg'>`;
       return;
     }
-    const rand = Math.floor(Math.random() * moles.length);
+    const rand = Math.floor(Math.random() * 8);
     let selected = moles[rand];
     selected.classList.remove("wgs__molehead--hidden");
-    setTimeout(hideMole, 1000, selected);
+    setTimeout(hideMole, timer, selected);
   }
 
   function hideMole(mole) {
     mole.classList.add("wgs__molehead--hidden");
-    setTimeout(popUpRandomMole, 1000);
+    turnsRemaining--;
+    remaining.innerHTML = `Moles remaining: ${turnsRemaining}`;
+    setTimeout(popUpRandomMole, timer);
   }
 
   popUpRandomMole();
